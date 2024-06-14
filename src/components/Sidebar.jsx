98a969bd64d691f383999775f0d46fde2b69a5dd
jaingam <jaingam.piyavhatkul@gmail.com>
2024-06-14 08:49:01 +0700

    fix: fix tablet pull down bug, selected section on the sidebar should display correctly style: add styling for .sidebar-menu-star other: add attribution

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -17,7 +17,7 @@ function Sidebar() {
             const sectionOffsetTop = section.offsetTop;
             const sectionHeight = section.offsetHeight;
 
-            if (scrollY >= sectionOffsetTop && scrollY < sectionOffsetTop + sectionHeight) {
+            if (scrollY >= sectionOffsetTop && scrollY < sectionOffsetTop + sectionHeight || scrollY <0 && section.id=='about') {
                 currentActiveMenu = section.id;
             }
         });
@@ -49,14 +49,18 @@ function Sidebar() {
     ">
         <div class="flex w-full flex-col justify-start">
             <a class={(activeMenu === 'about') ?
-                'sidebar-menu h-16 w-full pl-12 content-center text-slate-900 dark:text-slate-50 transition-colors' :
-                'sidebar-menu h-16 w-full pl-12 content-center text-slate-600 hover:text-slate-700 dark:text-slate-400 dark:hover:text-slate-200 transition-colors'
-            } href='#about'><span class={(activeMenu === 'about') ? "":"hidden"}>* </span>About</a>
+                'sidebar-menu flex flex-row h-16 w-full pl-12 content-center items-center text-slate-900 dark:text-slate-50 transition-colors' :
+                'sidebar-menu flex flex-row h-16 w-full pl-12 content-center items-center text-slate-600 hover:text-slate-700 dark:text-slate-400 dark:hover:text-slate-200 transition-colors'
+            } href='#about'>
+                <img class={(activeMenu === 'about') ? "sidebar-menu-star-selected h-max":"sidebar-menu-star-unselected h-max"} src="https://cdn-icons-png.flaticon.com/512/4510/4510931.png " width="16" height="16"></img> About
+
+                </a>
 
             <a class={(activeMenu === 'experience') ?
-                   'sidebar-menu h-16 w-full pl-12 content-center text-slate-900 dark:text-slate-50 transition-colors' :
-                   'sidebar-menu h-16 w-full pl-12 content-center text-slate-600 hover:text-slate-700 dark:text-slate-400 dark:hover:text-slate-200 transition-colors'
-                } href='#experience'><span class={(activeMenu === 'experience') ? "":"hidden"}>* </span>Experience</a>
+                   'sidebar-menu flex flex-row h-16 w-full pl-12 content-center items-center text-slate-900 dark:text-slate-50 transition-colors' :
+                   'sidebar-menu flex flex-row h-16 w-full pl-12 content-center items-center text-slate-600 hover:text-slate-700 dark:text-slate-400 dark:hover:text-slate-200 transition-colors'
+                } href='#experience'>
+                <img class={(activeMenu === 'experience') ? "sidebar-menu-star-selected h-max":"sidebar-menu-star-unselected h-max"} src="https://cdn-icons-png.flaticon.com/512/4510/4510931.png " width="16" height="16"></img> Experience</a>
             {/* <a class={(activeMenu === 'contact') ?
                 'sidebar-menu h-16 w-full pl-8 content-center bg-slate-400 hover:text-slate-900  dark:bg-slate-600 transition-colors' :
                 'sidebar-menu h-16 w-full pl-8 content-center hover:bg-slate-300 hover:text-slate-900 dark:hover:bg-slate-700 transition-colors'
--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -7,6 +7,7 @@ function Footer() {
             </div>
             <a class="text-slate-950 dark:text-slate-50 self-center" href="https://www.flaticon.com/free-icons/linkedin" title="linkedin icons">Linkedin icons created by Google - Flaticon</a>
             <a class="text-slate-950 dark:text-slate-50 self-center" href="https://www.flaticon.com/free-icons/cat" title="cat icons">Cat icons created by Dave Gandy - Flaticon</a>
+            <a href="https://www.flaticon.com/free-icons/asterisk" title="asterisk icons">Asterisk icons created by Mayor Icons - Flaticon</a>
         </div>
 )
 }
